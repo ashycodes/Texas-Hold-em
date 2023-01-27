eae4e75032f3d9c16a1cd250ef725cef1df23aae
ashycodes <64503359+ashycodes@users.noreply.github.com>
2023-01-26 20:06:56 -0600

    Updated html and css start page

--- app.js
+++ app.js
@@ -12,55 +12,63 @@ import {player} from "./Classes/player.js";
 const deck = new deckOfCards();
 const board = new gameBoard(deck);
 
-board.render()
 
-
-
-
+//board.render()
 
 //game manager class 
 //how any players
-//deals hands
-
 class gameManager {
+    constructor(activePlayers) {
+        this.activePlayersArr = []
+    }
+
+
+
+
+    get startGame() {
+        this.activePlayers.forEach(player => {
+            for (let i = 1; i <= activePlayers; i++) {
+                activePlayersArr.hand.unshift(deck.dealCard);
+            };
+
+    })
+
+};
+
+//startGame func - dealing out cards to players and accepting buyin
+//roundBetting func 
+//flop func
+//betting/turn phase 
+//turn
+//betting
+//river
+//betting
+//logic for players hands
+//split pot
+//clear players hands
+//shuffle new deck
+
+
+
 
 
 };
 
 
 
-const player1 = new player(1, "Ashy", 1000);
-const getCard = deck.dealCard;
-player1.hand.push(getCard)
-player1.hand.push(getCard)
-player1.hand.push(getCard)
 
+
+const player1 = new player(1, "Ashy", 1000);
+player1.hand.unshift(deck.dealCard);
+player1.hand.unshift(deck.dealCard);
 console.log(player1.hand);
 console.log(deck);
 
 
 
 
+const start = new gameManager(player1); 
+//console.log(start.startGame)
 
 
-//(carry over chips)
 
-//(buy in)
-
-//deal the cards (1 player no computer)
-
-//betting
-
-//reveal first 3 cards
-
-//betting/end
-
-//reaveal next card
-
-//final betting/end
-
-//reveal cards
-
-//decide winner based on hand
-
-//(calculate and disperse chips for next round)
